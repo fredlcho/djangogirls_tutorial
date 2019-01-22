544848770737d6c95446e6a454d9c85079cfa8e6
Frederick <fredericklcho@gmail.com>
2019-01-21 22:17:30 -0500

    pulls posts from DB and posts them on home page

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.post_list,name='post_list')
+    path('',views.post_list,name='post_list'),
+    path('testpage/',views.test_page,name='test_pageee')
 ]
